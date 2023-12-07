1e9a8e3b9b87e8831e4a135b6931bf8a6df3c44e
macvincent <macvincentaghaoko@gmail.com>
2023-12-07 13:18:13 -0800

    slow changes

--- hi_diff/models/HI_Diff_S1_model.py
+++ hi_diff/models/HI_Diff_S1_model.py
@@ -111,8 +111,8 @@ class HI_Diff_S1(BaseModel):
         self.optimizer_total.zero_grad()
 
         prior = self.net_le(self.lq, self.gt)
-        print(prior)
         self.output = self.net_g(self.lq, prior)
+        print(self.output.max(), self.gt.max())
 
         l_total = 0
         loss_dict = OrderedDict()
